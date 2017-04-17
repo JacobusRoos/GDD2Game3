7f50651b92c76c6906fef4033211439f91f37b75
JacobRoos <jxr5789@rit.edu>
2017-04-17 11:29:35 -0400

    sun rotates

---
+++ Assets/Scripts/SunScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<Transform>().Rotate(new Vector3(0, 10 * Time.deltaTime, 0));
+	}
+}
