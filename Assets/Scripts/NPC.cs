4cc9c9f699ee47d80e3f5876c502293e1c488610
JacobRoos <jxr5789@rit.edu>
2017-04-19 11:49:44 -0400

    NPC script started

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    public string NPCName;
+
+    private Dictionary<string, KeyValuePair<string, KeyValuePair<string, string>>> dialogue;
+
+    private int trust;
+
+    private bool yeti;
+
+	// Use this for initialization
+	void Start () {
+        NPCName = "";
+
+        dialogue = new Dictionary<string, KeyValuePair<string, KeyValuePair<string, string>>>();
+
+        trust = 0;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
