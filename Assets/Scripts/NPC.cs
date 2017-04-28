0a0111122f9e6739fa3e43c659525adb21a840ae
JacobRoos <jxr5789@rit.edu>
2017-04-28 11:48:03 -0400

    randomly selects dialogue ptions, specifically the index to be used in the UI

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,8 +11,18 @@ public class NPC : MonoBehaviour {
     private int trust;
 
     private bool yeti;
-    
+    //whether the prediction is true or false for the day
     private bool[] predictionStates;
+    
+    private bool[] predictionsUsed;
+    
+    private KeyValuePair<bool, bool>[] givenPredictions;
+    
+    private int givenPredictionNum;
+    
+    public int smallIndex;
+    public int trueIndex;
+    public int falseindex;
 
 	// Use this for initialization
 	void Start () {
@@ -24,6 +33,17 @@ public class NPC : MonoBehaviour {
         trust = 0;
 
         predictionStates = new bool[dialogue.predNum];
+        
+        predictionsUsed = new bool[dialogue.predNum];
+        
+        givenPredictions = new KeyValuePair<bool, bool>[3];
+        
+        givenPredictionNum = 0;
+        
+        trueIndex = -1;
+        falseindex = -1;
+        
+        RandomizePredictions();
 	}
 	
 	// Update is called once per frame
@@ -37,23 +57,59 @@ public class NPC : MonoBehaviour {
     {
         for(int i = 0; i < dialogue.predNum; i++)
         {
-            predictionState[i] = false;
+            predictionStates[i] = false;
+            predictionsUsed[i] = false;
         }
         
         bool[] assigned = new bool[dialogue.predNum];
-        Random r = new Random();
         
         for(int i = 0; i < dialogue.predNum / 2; i++)
         {
-            int index = r.Next(dialogue.predNum);
+            int index = (int)(Random.value * (dialogue.predNum - 1));
+            
             if(assigned[index])
             {
                 i--;
             }
             else
             {
-                predictionState[index] = true;
+                predictionStates[index] = true;
             }
         }
+    }
+    
+    public void DisplayDialogueOptions()
+    {
+        bool trueFound = false;
+        bool falseFound = false;
+        
+        while(!trueFound || !falseFound)
+        {
+            int r = (int)(Random.value * (dialogue.predNum - 1));
+            
+            if(predictionStates[r] && !predictionsUsed[r])
+            {
+                trueFound = true;
+                trueIndex = r;
+            }
+            
+            if(!predictionStates[r] && !predictionsUsed[r])
+            {
+                falseFound = true;
+                falseindex = r;
+            }
+        }
+        
+        smallIndex = (int)(Random.value * (dialogue.smallNum - 1));
+        
+        //Todo: put dialogue in the UI here
+        
+        //NOTE: please update predictionsUsed when a dialogue option is selected so the same one is not used multiple times
+        //also increment the 
+    }
+    
+    public void NextDay()
+    {
+        
     }
 }
